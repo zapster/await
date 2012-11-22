dbe77cd472651af6ed040ee2a9fda3954ff9d33e
Josef Eisl <zapster@zapster.cc>
2012-11-22 17:14:00 +0100

    await: fixed semaphore problem

--- src/main.c
+++ src/main.c
@@ -4,15 +4,39 @@
 #include <unistd.h>
 #include <sys/wait.h>
 #include <string.h>
+#include <signal.h>
 
 #include <fcntl.h>           /* For O_* constants */
 #include <sys/stat.h>        /* For mode constants */
 #include <semaphore.h>
 
-#define SEM_MODE S_IRWXU /* read, write, execute/search by owner */
+#define SEM_MODE S_IRWXU     /* read, write, execute/search by owner */
 #define SEM_VALUE 1
 
+#ifndef NDEBUG
+  #define DBG(...) \
+    fprintf (stderr, "%s:%d: ", __FILE__, __LINE__); \
+    fprintf (stderr, __VA_ARGS__)
+#else
+  #define DBG(...)
+#endif
+
 const char* arg0 = NULL;
+pid_t pid = 0;
+
+void sigint_handler (int signum)
+{
+  if (pid != 0)
+  {
+    /* parent */
+    if(kill(pid, signum) == -1)
+    {
+      fprintf(stderr,"%s: %s\n", arg0, strerror(errno));
+      abort();
+    }
+  }
+  signal(signum, sigint_handler);
+}
 
 inline static void usage()
 {
@@ -21,11 +45,10 @@ inline static void usage()
 
 inline static void child(int argc, char * argv[])
 {
-  /* TODO check argv*/
   char* e_arg0;
   char** e_argv;
   int i;
-  
+
   e_arg0 = argv[2];
   e_argv = (char**) calloc(argc - 2 + 1, sizeof(char*));
   /* TODO: check for error */
@@ -46,15 +69,14 @@ inline static void child(int argc, char * argv[])
 int main(int argc, char * argv[])
 {
   sem_t *sem;
-  pid_t pid;
   pid_t wpid;
   int status;
   char* semname;
   size_t len;
-  
+
   arg0 = argv[0];
 
-  if (argc < 3) 
+  if (argc < 3)
   {
     usage();
     return -1;
@@ -73,11 +95,14 @@ int main(int argc, char * argv[])
     fprintf(stderr,"%s: %s\n", arg0, strerror(errno));
     return -1;
   }
-  /* TODO: check for SEM_FAILED & errno */
-  //sem_close(sem);
-  /* TODO: return value & errno */
-  //sem_unlink(SEM_NAME);
-  /* TODO: return value & errno */
+
+  DBG("sem_wait()\n");
+  if (sem_wait(sem) == -1)
+  {
+    fprintf(stderr,"%s: %s\n", arg0, strerror(errno));
+    return -1;
+  }
+  DBG("entering mutex section\n");
 
   switch (pid = fork())
   {
@@ -85,14 +110,15 @@ int main(int argc, char * argv[])
       fprintf(stdout,"Can't fork child process!");
       return -1;
     case 0: /* Child */
-      if (sem_wait(sem) == -1)
+      child(argc, argv);
+      return -1; /* signal error to calling function */
+    default: /* Parent */
+      if (signal (SIGINT, sigint_handler) == SIG_ERR)
       {
         fprintf(stderr,"%s: %s\n", arg0, strerror(errno));
         return -1;
       }
-      child(argc, argv);
-      return -1; /* signal error to calling function */
-    default: /* Parent */
+
       while ((wpid = wait (&status)) != pid)
       {
         if (wpid != -1) /* other child terminated? */
@@ -105,16 +131,23 @@ int main(int argc, char * argv[])
         }
         return -1;
       }
-      if (sem_post(sem) == -1)
-      {
-        fprintf(stderr,"%s: %s\n", arg0, strerror(errno));
-        return -1;
-      }
-      if (sem_close(sem) == -1)
-      {
-        fprintf(stderr,"%s: %s\n", arg0, strerror(errno));
-        return -1;
-      }
+  }
+  DBG("sem_post()\n");
+  if (sem_post(sem) == -1)
+  {
+    fprintf(stderr,"%s: %s\n", arg0, strerror(errno));
+    return -1;
+  }
+  if (sem_close(sem) == -1)
+  {
+    fprintf(stderr,"%s: %s\n", arg0, strerror(errno));
+    return -1;
+  }
+  DBG("unlinking file: %s\n",semname);
+  if ((sem_unlink(semname) == -1) && (errno != ENOENT)) /* ignore if the semaphore does no longer exists */
+  {
+    fprintf(stderr,"%s: sem_unlink(%s): %s\n", arg0, semname, strerror(errno));
+    return -1;
   }
 
   return WEXITSTATUS(status);
